827c84ffacadbe5323742ecb4f4c39861c18d5db
MohaMBS <mohamoha144@gmail.com>
2020-10-08 16:50:29 +0200

    Primera parte del A3 acabada.

--- Clase/UF1/a3/pinta_formulari.php
+++ Clase/UF1/a3/pinta_formulari.php
@@ -1,8 +1,28 @@
+<?php
+if ($_SERVER['REQUEST_METHOD']== 'POST'){
+    error_reporting(0); //Cuando no llenas un capo se queja con noticie no es ningun error solo es info, entonces para que no slaga en la pagina lo que he hecho es desactivar los mensajes de errores.
+    if (isset($_REQUEST["mytext"]))
+        echo "Tu texto es: ";
+        print_r($_REQUEST["mytext"]);
+    if (isset($_REQUEST["myradio"]))
+        echo "<br>Tu radio buton es: ";
+        print_r($_REQUEST["myradio"]);
+    if (isset($_REQUEST["mycheckbox"]))
+        echo "<br>Has marcado los siguentes checkbox: ";
+        print_r($_REQUEST["mycheckbox"]);
+    if (isset($_REQUEST["myselect"]))
+        echo "<br>Has seleccionado el siguente item: ";
+        print_r($_REQUEST["myselect"]);
+    if (isset($_REQUEST["mytextarea"]))
+        echo "<br>Has escrito el siguente texto: ";
+        print_r($_REQUEST["mytextarea"]);
+}
+else{
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-
 <title>Exemple de formulari</title>
 
 </head>
@@ -11,7 +31,7 @@
 
 <div style="margin: 30px 10%;">
 <h3>My form</h3>
-<form action="processa_dades.php" method="post" id="myform" name="myform">
+<form action="pinta_formulari.php" method="post" id="myform" name="myform">
 
     <label>Text</label> <input type="text" value="" size="30" maxlength="100" name="mytext" id="" /><br /><br />
 
@@ -39,6 +59,8 @@ Text area
 
 </form>
 </div>
-
 </body>
 </html>
+<?php   
+}
+?>
