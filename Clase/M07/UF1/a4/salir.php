600b6893dec6e98954ad8d93afec36b4dc11e822
MohaMBS <mohamoha144@gmail.com>
2020-10-25 16:13:47 +0100

    Corregido un fallo, si en privada ponias.php?salgo, te hacia el logout y lo he corregido para que no se pueda.

--- Clase/M07/UF1/a4/salir.php
+++ Clase/M07/UF1/a4/salir.php
@@ -1,12 +1,14 @@
 <?php 
-if (isset($_REQUEST["salgo"])){
-    if ($_REQUEST["salgo"]="salir"){
-        session_unset();
-        session_destroy();
-        $_SESSION=null;
-        header('Location: login.php');
-    }else{
-        header('Location: privada.php');
+if ($_SERVER["REQUEST_METHOD"]== "POST"){
+    if (isset($_REQUEST["salgo"])){
+        if ($_REQUEST["salgo"]="salir"){
+            session_unset();
+            session_destroy();
+            $_SESSION=null;
+            header('Location: login.php');
+        }else{
+            header('Location: privada.php');
+        }
     }
 }
 ?>
